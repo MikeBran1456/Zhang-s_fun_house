57ffefb188c956cc626e3775ff551ca87d50e291
porter235 <31319959+porter235@users.noreply.github.com>
2017-10-08 16:34:47 -0400

    Fresh upload
    Zhang's outline was making eclipse very unhappy so I cleaned most of it up that I needed. Everything is a work in progress right now.

--- src/IOdevice.java
+++ src/IOdevice.java
@@ -1,11 +1,18 @@
-Class IOdevice{
-	Public IOdevice(ArrayList<Process> Wait_Queue)
+package simulation;
+
+import java.util.ArrayList;
+
+class IOdevice {
+	public IOdevice(ArrayList<Process> Wait_Queue)
+
 	public boolean BusyOrNot;
-	//Always pick one process from Wait_Queue to exeute
-	public String execute(int IO_burst){
-	BusyOrNot=true;
-	//Call Bubble Sort() for IO_burst times and then return â€œreadyâ€�;
+
+	// Always pick one process from Wait_Queue to exeute
+	public String execute(int IO_burst) {
+		BusyOrNot = true;
+		// Call Bubble Sort() for IO_burst times and then return ;
 	}
-	Public BubbleSort(){}
+
+	public static void BubbleSort() {
 	}
 }
